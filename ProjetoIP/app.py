28997c2cbaf9e8682acb5d6595b22a38982d1f38
Marcelle Castro <131879933+marczlle@users.noreply.github.com>
2024-10-02 17:51:47 -0300

    Update app.py

--- ProjetoIP/app.py
+++ ProjetoIP/app.py
@@ -4,6 +4,15 @@ import time
 
 users = {}
 
+# cores para o personagem
+
+CYAN = "\033[36m"
+RED = "\033[31m"
+BLUE = "\033[34m"
+YELLOW = "\033[33m"
+RESET = "\033[0m" 
+
+
 # Função para criar um novo usuário
 def criar_novo_usuario(email, senha):
     if email in users:
@@ -28,6 +37,8 @@ def login(email, senha):
         print("Login realizado com sucesso!\n")
     else:
         print("E-mail ou senha incorretos. Tente novamente.")
+        quit()
+
 
 def editar_personagem(usuario):
     personagem = users[usuario]['personagem']
@@ -77,11 +88,11 @@ def exibir_personagem(usuario):
 
         # iterando sobre as linhas do personagem
     for row in character:
-        if "####" in row:  # Condição simplificada para detectar o cabelo
-            print(f"{RED}{row}{RESET}")
-        elif "++" in row:
+        if "#" in row:  # Condição simplificada para detectar o cabelo
+            print(f"{CYAN}{row}{RESET}")
+        elif "+" in row:
             print(f"{BLUE}{row}{RESET}")
-        elif "&&&&&&&&&&" in row:
+        elif "$" in row:
             print(f"{YELLOW}{row}{RESET}")
         else:
             print(row)
@@ -89,7 +100,8 @@ def exibir_personagem(usuario):
 # acabaram as funções
 
 # entrando no loop inicial do programa (cadastro e login)
-while True:#
+while True:
+    print("O que você quer fazer? ")
     print("\n1. Criar novo usuário \n2. Fazer login \n3. Cancelar")
     escolha = input("Escolha uma opção: ")
 
@@ -99,12 +111,7 @@ while True:#
         novo_email = input("Digite o novo email: ")
         nova_senha = input("Digite a nova senha: ")
         criar_novo_usuario(novo_email, nova_senha)
-        time.sleep(0.7)
-        print("\nBem-vindo ao sistema! Faça o login:")
-        email_login = input("\tDigite seu email: ")
-        senha_login = input("\tDigite sua senha: ")
-        login(email_login, senha_login)
-        break
+        time.sleep(0.3)
 
     elif escolha == '2':
         # Input de email e senha para login
@@ -124,12 +131,6 @@ while True:#
     else:
         print("Opção inválida! Tente novamente.")
 
-# cores para o personagem
-CYAN = "\033[36m"
-RED = "\033[31m"
-BLUE = "\033[34m"
-YELLOW = "\033[33m"
-RESET = "\033[0m" 
 
 print(f"Olá, {nome_usuario}! Está pronto para novas aventuras?")
 # loop para opções do jogador já logado/cadastrado no sistema
@@ -144,7 +145,7 @@ while True:
         if escolha_personagem == "1": 
             exibir_personagem(email_login)
         if escolha_personagem == "2":
-            editar_personagem()
+            editar_personagem(email_login)
                 
     elif escolha == '2':
         print("Iniciando o jogo", end="") 
